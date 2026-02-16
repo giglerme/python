9eea9f5e9521244ab980dddb1b551feacca6fb0a
giglerme <dantasmguilherme@gmail.com>
2026-02-16 16:23:52 -0300

    ex025 posições +1

--- PythonExercicios/ex026.py
+++ PythonExercicios/ex026.py
@@ -1,4 +1,4 @@
 frase = str(input('Digite uma frase: ')).strip().upper()
 print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
-print('A letra A aparece pela primeira vez na {} posição.'.format(frase.find('A')))
-print('A letra A aparece pela ultima vez na {} posição.'.format(frase.rfind('A')))
+print('A letra A aparece pela primeira vez na {} posição.'.format(frase.find('A') + 1))
+print('A letra A aparece pela ultima vez na {} posição.'.format(frase.rfind('A') + 1))
