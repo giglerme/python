c766700375f7035f8a3cea16582492710e5f2e50
giglerme <dantasmguilherme@gmail.com>
2026-02-18 00:24:04 -0300

    exercicio 33 concluído

--- PythonExercicios/ex033.py
+++ PythonExercicios/ex033.py
@@ -4,8 +4,9 @@ n3 = int(input('Digite o terceiro valor: '))
 nums = [n1, n2, n3].sort()
 
 if n1 > n2 and n1 > n3 :
-    print('{} é o maior número.'.format(n1))
+    print('{} é o maior número.'.format(n1), 'e {} é o menor.'.format(n3))
+
 elif n2> n3:
-    print('{} é o maior número.'.format(n2))
+    print('{} é o maior número.'.format(n2), 'e {} é o menor.'.format(n1))
 else:
-    print('{} é o maior número.'.format(n3))
+    print('{} é o maior número, e {} é o menor.'.format(n3, n2))
