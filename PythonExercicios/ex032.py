436c67c6b0b3d695f56b940bc6726acc1779d1c6
giglerme <dantasmguilherme@gmail.com>
2026-02-19 01:54:55 -0300

    ex033(alternativo) diferenciar 3 números por comparação

--- PythonExercicios/ex032.py
+++ PythonExercicios/ex032.py
@@ -1,5 +1,8 @@
+from datetime import date
 ano = int(input('Qual ano gostaria de saber se é ou não bissexto? '))
-if ano % 4 == 0 :
-    print('É bissexto')
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano != 100 or ano % 400 == 0 :
+    print('{} é bissexto'.format(ano))
 else:
-    print('Não é bissexto')
+    print('{} não é bissexto'.format(ano))
